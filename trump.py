9cff364cb70e937f123b0c662aec6c0e81212e32
brtsai <brtsai@ucsc.edu>
2017-06-28 12:43:33 -0700

    added timestamps to posts

--- trump.py
+++ trump.py
@@ -1,5 +1,6 @@
 import urllib2
 import json
+from datetime import datetime
 from bs4 import BeautifulSoup
 
 #read the access token in from AccessToken the file
@@ -8,8 +9,7 @@ with open("AccessToken") as f:
 host = "https://graph.facebook.com"
 api_version = "v2.9"
 id_to_request = "DonaldTrump"
-fields_to_request = ""
-fields_request = "fields=posts.limit(20)" + fields_to_request + "&"
+fields_request = "fields=posts.limit(20)&created_time&"
 
 my_request = host + "/" + api_version + "/" + id_to_request + "?" + fields_request + "access_token=" + access_token
 
@@ -19,7 +19,8 @@ parsed_json = json.loads(json_reply)
 
 print "Printing 20 most recent Trump post blurbs:"
 for x in range(0,len(parsed_json["posts"]["data"])) :
-    print "[" + str(1+x) + "] " + BeautifulSoup(parsed_json["posts"]["data"][x]["message"], "html.parser").decode("utf-8")
+    print "[" + str(1+x) + " " + parsed_json["posts"]["data"][x]["created_time"] + "]"
+    print BeautifulSoup(parsed_json["posts"]["data"][x]["message"], "html.parser").decode("utf-8")
 
 
 
